3b7b0595602d242ed0b3213f1a49d2fb6d2a6c51
Dom Sip <dominik.sipowicz@gmail.com>
2022-01-21 18:40:03 +0000

    test AMP hybrid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+export config = { amp: 'hybrid' };
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
